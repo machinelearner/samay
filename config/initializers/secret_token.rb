108ccc8f049d3c001324fa0812a8cb398beb04ad
Pramod N <npramod05@gmail.com>
2013-08-12 11:34:27 +0530

    K1/Pramod | initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Samay::Application.config.secret_key_base = '2d884eee209ea1064bf6d1814ce3bdb6a3cacc18dfb6f2a5c19404b85326180ad9719cc054b8c301876b6859ae6d139bc64851668bad0ac5b5a11a0ce086c995'
